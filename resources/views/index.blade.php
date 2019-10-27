f8c47b6cc19ed12890f48442ce490d825fbb772d
khoa <nkhoanguyen2703@gmail.com>
2019-10-27 10:40:20 +0700

    Add some views, add map

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,14 +14,14 @@
             <!-- Page Heading -->
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">
+                    <!-- <h1 class="page-header">
                         Dashboard <small>Overview</small>
                     </h1>
                     <ol class="breadcrumb">
                         <li class="active">
                             <i class="fa fa-dashboard"></i> Dashboard
                         </li>
-                    </ol>
+                    </ol> -->
                 </div>
             </div>
             <!-- /.row -->
--- resources/views/locations/locations.blade.php
+++ resources/views/locations/locations.blade.php
@@ -68,6 +68,10 @@
                     </div>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=C%E1%BA%A7n%20Th%C6%A1%20university&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/ipvanish-coupon/">embedgooglemap.net</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:100%;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:100%;}</style></div>
+            </div>
         </div>
         <!-- /.container-fluid -->
 
